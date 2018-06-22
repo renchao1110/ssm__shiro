717f171075eab3487e4435dc10bed785a275c3ac
renchao1110 <583816993@qq.com>
2018-06-22 19:12:15 +0800

    项目搭建完成，开发登录模块

--- src/main/java/sysUser/controller/LoginController.java
+++ src/main/java/sysUser/controller/LoginController.java
@@ -1,13 +1,26 @@
 package sysUser.controller;
 
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-    public String login(){
-        System.out.println(1111111);
+    public String login(HttpServletRequest request){
+        String exceptionClassName = request.getParameter("shiroLoginFailure");
+        if(exceptionClassName!=null){
+            if (UnknownAccountException.class.getName().equals(exceptionClassName)){
+                throw new RuntimeException("用户账号不存在！！！！");
+            }else if (IncorrectCredentialsException.class.getName().equals(exceptionClassName)){
+                throw new RuntimeException("账户或密码错误");
+            }else {
+                throw new RuntimeException("出现其他错误登录失败");
+            }
+        }
         return "login";
     }
 }
